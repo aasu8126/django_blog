692e2cbda73341a6d9bb032a10276e8f96d15707
Aasu <aasu@ordertrainings.com>
2018-12-22 20:23:58 +0530

    lesson 17 uploading media

--- djangoautic/urls.py
+++ djangoautic/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^articles/',include('articles.urls')),  # articles.urls == app name . urls file name
@@ -27,3 +30,5 @@ urlpatterns = [
 
 
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
